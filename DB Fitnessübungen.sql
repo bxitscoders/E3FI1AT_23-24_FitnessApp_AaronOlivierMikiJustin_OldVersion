bdbb96a1eb0e13977d8fdf443a59c16cad2d7d6e
MihailoSimvic <98316639+mihailosimovic@users.noreply.github.com>
2024-02-08 11:14:09 +0100

    Add files via upload

---
+++ DB Fitnessübungen.sql
@@ -0,0 +1,44 @@
+insert into fitnessübungen(id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (1,'Kniebeugen','Von Standposition in Sitzposition verlagern und wieder zurück','Kraftausdauer', 1, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (2,'Liegestütze','Hände und Beine auf dem Boden festigen und zeitgleich den Oberkörper wie ein Brett.','Kraft', 2, 20);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (3,'Hampelmann','In Standposition gehen und die Arme hoch und runter bewegen. Währenddessen die Beine musst du auseinander trennen und wieder zusammen tun.','Ausdauer', 1, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (4,'Sit ups','Lege dich auf den Rücken und stelle deine Beine etwa schulterbreit auf, die Beine sollten dabei etwa im 90-Grad-Winkel gebeugt sein. Verschränke deine Hände hinter dem Kopf (oder kreuze sie alternativ vor der Brust) und spanne deine Bauchmuskeln an. Dann hebst du deinen Oberkörper langsam an und richtest dich auf.','Kraft', 2, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (5,'Burpees','Stell dich hüftbreit hin, die Zehen zeigen gerade nach vorn. Nun gehst du tief in die Hocke und stützt dich mit den Händen vor den Füßen auf dem Boden ab. Deine Hände sind dabei schulterbreit auseinander. Spring nun in die Liegestützposition und führe einen Liegestütz aus. Jetzt springst du mit den Füßen wieder nach vorn, sodass du erneut in Hockposition bist. Zum Abschluss führst du einen Sprung nach oben aus. Deine Arme hast du dabei über dem Kopf ausgestreckt. Nach deinem Hockstrecksprung landest du wieder mit hüftbreit geöffneten Beinen auf dem Boden und befindest dich wieder in der Anfangsposition.  ','Kraftausdauer', 3, 20);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (6,'Skipping','Nimm ein Seil und halte mit deinen Händen an den Enden des Seiles. Anschließend fang an das Seil um dich zu drehen und spring über den rundenen Seil.','Ausdauer', 1, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (7,'Planking','Lege dich auf die Seite und stütze dich auf deinem Unterarm ab. Die Ellenbogen sind unter den Schultern, der Unterarm senkrecht zum Körper, Handfläche auf dem Boden, Füße liegen übereinander. Presse deinen Unterarm in den Boden und strecke die Hüften Richtung Decke nach oben.','Kraft', 2, 20);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (8,'Planking seitlich','Achten Sie dabei darauf, dass Ellenbogen und Schulter auf einer Linie bleiben. Heben Sie das Becken so weit an, dass es zusammen mit Ihrem Oberkörper und den Beinen eine gerade Linie bildet und halten Sie die Position solange Sie können. Wechseln Sie anschließend die Seite.','Kraft', 2, 20);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (9,'Radfahren','Heben Sie ein Bein an bis Hüfte und Knie im 90 Grad Winkel stehen. Bringen Sie Ihr anderes Bein in die gleiche Position und halten Sie den hüftbreiten Abstand. Halten Sie den Rumpf stabil, während Sie mit beiden Beinen Radfahren. Je weiter Sie Ihre Füße vom Körper wegstrecken, desto schwieriger wird die Übung.','Ausdauer', 1, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (10,'Joggen','Fangen Sie langsam an. Laufen Sie keine langen Strecken. Machen Sie sich nicht zu viele Gedanken über die Technik. Legen Sie Ruhetage ein. Wählen Sie die richtigen Laufschuhe. Tragen Sie Funktionskleidung.','Ausdauer', 1, 900);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (11,'Pinkelnder Hund','Ausgangsposition: Du befindest Dich in einem Vierfüßlerstand. Bewegungsausführung: Hebe nun das rechte Bein hoch und führe es angewinkelt nach rechts oben. Danach senkst Du das Bein wieder, stützt es aber nicht wieder auf dem Boden ab, sondern streckst es direkt nach hinten oben aus.','Kraftausdauer', 2, 20);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (12,'Bergsteiger','Begib dich in eine Liegestütz-Position. Bauchmuskeln fest anspannen und die Hüfte möglichst unten halten. Ziehe nun abwechselnd die Knie zur Brust und führe diese Bewegung so schnell wie möglich und mit Kontrolle aus.','Kraftausdauer', 3, 20);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (13,'High Knees','Ausgangsposition: Stell dich schulterbreit hin und mach dich locker. Bewegung: Abwechselnd ziehst du jetzt deine Knie bis auf Hüfthöhe hoch (nicht mogeln!).','Ausdauer', 2, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (14,'Liegestütze zu Planks','Stellen Sie die Zehen auf und stützen Sie sich darauf ab. Machen Sie Ihren Körper steif wie ein Brett: Knie und Hüfte sind nun ganz durchgestreckt, Gesäß und Bauchmuskulatur angespannt (Nabel einziehen). Ihr gesamter Körper sollte eine gerade Linie bilden!','Kraftausdauer', 3, 30);
+
+insert into fitnessübungen (id, name, beschreibung, kategorie, schwierigkeitsgrad, zeit)
+values (15,'Tuck Jumps','Die Füße stehen hüftbreit, die Brust ist aufrecht. Schiebe deine Hüften nach hinten und unten (ähnlich, wie bei einem halben Squat). Zieh die Arme hoch und stoße dich mit deiner gesammelten Sprungkraft vom Boden ab.','Kraftausdauer', 2, 30);
